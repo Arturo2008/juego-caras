b281e0d3d7547d7aa674acfd27f4b7f55a7cb112
Arturo2008 <124789986+Arturo2008@users.noreply.github.com>
2023-02-21 11:27:16 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,72 @@
-function iniciar () {
-    if (randint(1, 5) == 1) {
-        basic.showIcon(IconNames.Heart)
-    }
-    if (randint(1, 5) == 2) {
-        basic.showIcon(IconNames.Yes)
-    }
-    if (randint(1, 5) == 3) {
-        basic.showIcon(IconNames.No)
-    }
-    if (randint(1, 5) == 4) {
-        basic.showIcon(IconNames.Sad)
-    }
-    if (randint(1, 5) == 5) {
-        basic.showIcon(IconNames.Skull)
-    }
-    if (randint(1, 5) == 6) {
-        basic.showIcon(IconNames.Happy)
-    }
-}
 input.onButtonPressed(Button.A, function () {
-	
+    if (pulsado == false) {
+        pulsado = true
+        if (num_icono == 0 || num_icono == 2) {
+            puntos_a += 1
+            if (puntos_a == 10) {
+                basic.showString("A: has ganado")
+                music.playMelody("C D C F C A C C5 ", 500)
+            }
+        } else if (num_icono == 0) {
+            basic.showString("A: has perdido")
+            basic.pause(5000)
+        } else {
+            if (puntos_a > 0) {
+                puntos_a += -1
+            }
+        }
+    }
+})
+function partida_nueva () {
+    juego_detenido = false
+    puntos_a = 0
+    puntos_b = 0
+    pulsado = false
+}
+input.onButtonPressed(Button.AB, function () {
+    partida_nueva()
 })
 input.onButtonPressed(Button.B, function () {
-	
+    if (pulsado == false) {
+        pulsado = true
+        if (num_icono == 0 || num_icono == 2) {
+            puntos_b += 1
+        } else if (num_icono == 0) {
+            basic.showString("A: has perdido")
+            basic.pause(5000)
+        } else {
+            if (puntos_b > 0) {
+                puntos_b += -1
+            }
+        }
+    }
 })
-let elegir_icono = 1
+function cambia_icono () {
+    icono = randint(0, 9)
+    emogis[icono].showImage(0)
+    basic.pause(200)
+}
+let icono = 0
+let puntos_b = 0
+let juego_detenido = false
+let puntos_a = 0
+let num_icono = 0
+let pulsado = false
+let emogis: Image[] = []
+emogis = [
+images.iconImage(IconNames.Angry),
+images.iconImage(IconNames.No),
+images.iconImage(IconNames.House),
+images.iconImage(IconNames.Yes),
+images.iconImage(IconNames.Angry),
+images.iconImage(IconNames.Giraffe),
+images.iconImage(IconNames.Heart),
+images.iconImage(IconNames.Umbrella),
+images.iconImage(IconNames.Heart),
+images.iconImage(IconNames.Ghost)
+]
+basic.forever(function () {
+    if (!(juego_detenido)) {
+        cambia_icono()
+    }
+})
